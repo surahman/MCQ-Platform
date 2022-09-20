383abb4f7d4bacc4f670e7c2bbd21e086a3b69ea
Saad Ur Rahman <saadurrahman@apache.org>
2022-09-20 18:21:36 -0400

    [Utilities] ErrorValidation struct for all errors found in a struct.

--- pkg/utilities/validator.go
+++ pkg/utilities/validator.go
@@ -1,6 +1,9 @@
 package utilities
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 // ErrorField contains information on JSON validation errors.
 type ErrorField struct {
@@ -12,4 +15,18 @@ type ErrorField struct {
 // Error will output the validation error for a single structs data member.
 func (err *ErrorField) Error() string {
 	return fmt.Sprintf("Field: %s, Tag: %s, Value: %s\n", err.Field, err.Tag, err.Value)
+}
+
+// ErrorValidation contains all the validation errors found in a struct.
+type ErrorValidation struct {
+	Errors []*ErrorField `json:"validation_errors" yaml:"validation_errors"` // A list of all data members that failed validation.
+}
+
+// Error will output the validation error for all struct data members.
+func (err *ErrorValidation) Error() string {
+	var buffer bytes.Buffer
+	for _, item := range err.Errors {
+		buffer.WriteString(item.Error())
+	}
+	return buffer.String()
 }
--- pkg/utilities/validator_test.go
+++ pkg/utilities/validator_test.go
@@ -1,6 +1,7 @@
 package utilities
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 
@@ -34,6 +35,54 @@ func TestErrorField_Error(t *testing.T) {
 			"Field, Tag, and Value error",
 			&ErrorField{Field: "field", Tag: "tag", Value: "value"},
 			fmt.Sprintf(errorStr, "field", "tag", "value"),
+		},
+		// ----- test cases end ----- //
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			require.Equal(t, testCase.expected, testCase.input.Error())
+		})
+	}
+}
+
+func TestErrorValidation_Error(t *testing.T) {
+
+	genExpected := func(errs ...string) string {
+		var buffer bytes.Buffer
+		for _, item := range errs {
+			buffer.WriteString(item)
+		}
+		return buffer.String()
+	}
+
+	const errorStr = "Field: %s, Tag: %s, Value: %s\n"
+
+	testCases := []struct {
+		name     string
+		input    *ErrorValidation
+		expected string
+	}{
+		// ----- test cases start ----- //
+		{
+			"Empty error",
+			&ErrorValidation{Errors: []*ErrorField{}},
+			genExpected(""),
+		},
+		{
+			"Single error",
+			&ErrorValidation{Errors: []*ErrorField{
+				{Field: "field 1", Tag: "tag 1", Value: "value 1"},
+			}},
+			genExpected(fmt.Sprintf(errorStr, "field 1", "tag 1", "value 1")),
+		},
+		{
+			"Two errors",
+			&ErrorValidation{Errors: []*ErrorField{
+				{Field: "field 1", Tag: "tag 1", Value: "value 1"},
+				{Field: "field 2", Tag: "tag 2", Value: "value 2"},
+			}},
+			genExpected(fmt.Sprintf(errorStr, "field 1", "tag 1", "value 1"),
+				fmt.Sprintf(errorStr, "field 2", "tag 2", "value 2")),
 		},
 		// ----- test cases end ----- //
 	}
