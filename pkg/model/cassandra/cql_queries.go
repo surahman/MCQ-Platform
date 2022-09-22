d30c160b70fa280e06dcb622ce36f6e5ef757d4a
Saad Ur Rahman <saadurrahman@apache.org>
2022-09-22 17:37:59 -0400

    [Model] Cassandra user account CRD queries.

--- pkg/model/cassandra/cql_queries.go
+++ pkg/model/cassandra/cql_queries.go
@@ -2,13 +2,13 @@ package model_cassandra
 
 const (
 	// CreateKeyspace sets up the keyspace in the cluster.
-	// Param 1: keyspace name
-	// Param 2: replication strategy
-	// Param 4: replication factor
+	// String Param 1: keyspace name
+	// String Param 2: replication strategy
+	// String Param 4: replication factor
 	CreateKeyspace = `CREATE KEYSPACE IF NOT EXISTS $s WITH replication = {'class' : '$s', 'replication_factor' : $d};`
 
 	// CreateUsersTable creates the Users table.
-	// Param 1: keyspace name
+	// String Param 1: keyspace name
 	CreateUsersTable = `CREATE TABLE IF NOT EXISTS %s.users (
     account_id  text,
     username    text,
@@ -21,7 +21,7 @@ const (
 );`
 
 	// CreateQuestionUDT creates the Question UDT that is used by the Questions table. CreateQuestionUDT must be called after this statement.
-	// Param 1: keyspace name
+	// String Param 1: keyspace name
 	CreateQuestionUDT = `CREATE TYPE IF NOT EXISTS $s.question (
     description text,
     options     list<text>,
@@ -29,7 +29,7 @@ const (
 );`
 
 	// CreateQuizzesTable creates the Quizzes table. CreateQuestionUDT must be called before this statement.
-	// Param 1: keyspace name
+	// String Param 1: keyspace name
 	CreateQuizzesTable = `CREATE TABLE IF NOT EXISTS %s.quizzes (
     quiz_id         uuid,                           // Unique identifier for the quiz.
     author          text,                           // Username of the quiz creator.
@@ -41,7 +41,7 @@ const (
 );`
 
 	// CreateResponsesTable creates the Responses table. CreateResponsesIndex must be called after this statement.
-	// Param 1: keyspace name
+	// String Param 1: keyspace name
 	CreateResponsesTable = `CREATE TABLE IF NOT EXISTS %s.responses (
     username text,
     quiz_id uuid,
@@ -54,4 +54,30 @@ const (
 	// CreateResponsesTable must be called before this statement.
 	// Param 1: keyspace name
 	CreateResponsesIndex = `CREATE INDEX responses_statistics_index ON %s.responses (quiz_id);`
+
+	// -----   Users Table Queries   -----
+
+	// CreateUser inserts a new user row into the Users table if it does not already exist.
+	// String Param 1: keyspace name
+	// Query Params: username, account_id, password, first_name, last_name, email, is_deleted
+	CreateUser = `INSERT INTO %s.users (username, account_id, password, first_name, last_name, email, is_deleted)
+VALUES (?, ?, ?, ?, ?, ?, ?)
+IF NOT EXISTS;`
+
+	// ReadUser retrieves a user account row from the Users table.
+	// String Param 1: keyspace name
+	// Query Params: username, account_id
+	ReadUser = `SELECT * FROM %s.users WHERE username = ? AND account_id = ?;`
+
+	// DeleteUser will mark a users account as deleted.
+	// String Param 1: keyspace name
+	// Query Params: username, account_id
+	DeleteUser = `UPDATE %s.users
+SET is_deleted = true
+WHERE username = ? AND account_id = ? IF EXISTS;`
+
+	// -----   Quizzes Table Queries   -----
+
+	// -----   Responses Table Queries   -----
+
 )
