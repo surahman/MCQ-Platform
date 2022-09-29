37758ddf72fc1b30159a16a9394156d8f0a15554
Saad Ur Rahman <saadurrahman@apache.org>
2022-09-29 16:57:30 -0400

    [Models] Cassandra queries removed format strings.

--- pkg/model/cassandra/cql_queries.go
+++ pkg/model/cassandra/cql_queries.go
@@ -5,11 +5,10 @@ const (
 	// String Param 1: keyspace name
 	// String Param 2: replication strategy
 	// String Param 4: replication factor
-	CreateKeyspace = `CREATE KEYSPACE IF NOT EXISTS $s WITH replication = {'class' : '$s', 'replication_factor' : $d};`
+	CreateKeyspace = `CREATE KEYSPACE IF NOT EXISTS %s WITH replication = {'class' : '%s', 'replication_factor' : %d};`
 
 	// CreateUsersTable creates the Users table.
-	// String Param 1: keyspace name
-	CreateUsersTable = `CREATE TABLE IF NOT EXISTS %s.users (
+	CreateUsersTable = `CREATE TABLE IF NOT EXISTS users (
     account_id  text,
     username    text,
     password    text,
@@ -21,8 +20,7 @@ const (
 );`
 
 	// CreateQuestionUDT creates the Question UDT that is used by the Questions table. CreateQuestionUDT must be called after this statement.
-	// String Param 1: keyspace name
-	CreateQuestionUDT = `CREATE TYPE IF NOT EXISTS $s.question (
+	CreateQuestionUDT = `CREATE TYPE IF NOT EXISTS question (
     description text,
     asset       text,
     options     list<text>,
@@ -30,8 +28,7 @@ const (
 );`
 
 	// CreateQuizzesTable creates the Quizzes table. CreateQuestionUDT must be called before this statement.
-	// String Param 1: keyspace name
-	CreateQuizzesTable = `CREATE TABLE IF NOT EXISTS %s.quizzes (
+	CreateQuizzesTable = `CREATE TABLE IF NOT EXISTS quizzes (
     quiz_id         uuid,
     author          text,
     title           text,
@@ -43,8 +40,7 @@ const (
 );`
 
 	// CreateResponsesTable creates the Responses table. CreateResponsesIndex must be called after this statement.
-	// String Param 1: keyspace name
-	CreateResponsesTable = `CREATE TABLE IF NOT EXISTS %s.responses (
+	CreateResponsesTable = `CREATE TABLE IF NOT EXISTS responses (
     username text,
     quiz_id uuid,
     score double,
@@ -54,83 +50,69 @@ const (
 
 	// CreateResponsesIndex creates an index on the Responses table to support looking up statistics based on the quiz_id.
 	// CreateResponsesTable must be called before this statement.
-	// Param 1: keyspace name
-	CreateResponsesIndex = `CREATE INDEX responses_statistics_index ON %s.responses (quiz_id);`
+	CreateResponsesIndex = `CREATE INDEX responses_statistics_index ON responses (quiz_id);`
 
 	// -----   Users Table Queries   -----
 
 	// CreateUser inserts a new user record into the Users table if it does not already exist.
-	// String Param 1: keyspace name
 	// Query Params: username, account_id, password, first_name, last_name, email, is_deleted
-	CreateUser = `INSERT INTO %s.users (username, account_id, password, first_name, last_name, email, is_deleted)
+	CreateUser = `INSERT INTO users (username, account_id, password, first_name, last_name, email, is_deleted)
 VALUES (?, ?, ?, ?, ?, ?, ?)
 IF NOT EXISTS;`
 
 	// ReadUser retrieves a user account record from the Users table.
-	// String Param 1: keyspace name
 	// Query Params: username, account_id
-	ReadUser = `SELECT * FROM %s.users WHERE username = ? AND account_id = ?;`
+	ReadUser = `SELECT * FROM users WHERE username = ? AND account_id = ?;`
 
 	// DeleteUser will mark a users account as deleted.
-	// String Param 1: keyspace name
 	// Query Params: username, account_id
-	DeleteUser = `UPDATE %s.users
+	DeleteUser = `UPDATE users
 SET is_deleted = true
 WHERE username = ? AND account_id = ? IF EXISTS;`
 
 	// -----   Quizzes Table Queries   -----
 
 	// CreateQuiz inserts a new Quiz record into the Quizzes table if it does not already exist.
-	// String Param 1: keyspace name
 	// Query Params: quiz_id, author, title, questions, marking_type, is_published, is_deleted
-	CreateQuiz = `INSERT INTO %s.quizzes (quiz_id, author, title, questions, marking_type, is_published, is_deleted)
+	CreateQuiz = `INSERT INTO quizzes (quiz_id, author, title, questions, marking_type, is_published, is_deleted)
 VALUES (?, ?, ?, ?, ?, false, false)
 IF NOT EXISTS ;`
 
 	// ReadQuiz retrieves a Quiz record from the Quizzes table.
-	// String Param 1: keyspace name
 	// Query Params: quiz_id,
-	ReadQuiz = `SELECT * FROM %s.quizzes WHERE quiz_id = ?;`
+	ReadQuiz = `SELECT * FROM quizzes WHERE quiz_id = ?;`
 
 	// UpdateQuiz updates a Quiz record in the Quizzes table if it is not published.
-	// String Param 1: keyspace name
 	// Query Params: title, questions, marking_type, quiz_id
-	UpdateQuiz = `UPDATE %s.quizzes
+	UpdateQuiz = `UPDATE quizzes
 SET title = ?, questions = ?, marking_type = ?
 WHERE quiz_id = ? IF is_published = false;`
 
 	// DeleteQuiz marks a Quiz record as deleted in the Quizzes table.
-	// String Param 1: keyspace name
 	// Query Params: quiz_id
-	DeleteQuiz = `UPDATE %s.quizzes
+	DeleteQuiz = `UPDATE quizzes
 SET is_deleted = true
 WHERE quiz_id = ? IF EXISTS;`
 
 	// PublishQuiz marks a Quiz record as published in the Quizzes table.
-	// String Param 1: keyspace name
 	// Query Params: quiz_id
-	PublishQuiz = `UPDATE %s.quizzes
+	PublishQuiz = `UPDATE quizzes
 SET is_published = true
 WHERE quiz_id = ? IF EXISTS;`
 
 	// -----   Responses Table Queries   -----
 
 	// CreateResponse inserts a new Response record into the Quizzes table if it does not already exist.
-	// String Param 1: keyspace name
 	// Query Params: username, quiz_id, score, responses
-	CreateResponse = `INSERT INTO %s.responses (username, quiz_id, score, responses)
+	CreateResponse = `INSERT INTO responses (username, quiz_id, score, responses)
 VALUES (?, ?, ?, ?)
 IF NOT EXISTS;`
 
 	// ReadResponse retrieves a Response record from the Responses table.
-	// String Param 1: keyspace name
 	// Query Params: username, quiz_id
-	ReadResponse = `SELECT * FROM %s.responses
-WHERE username = ? AND quiz_id = ?;`
+	ReadResponse = `SELECT * FROM responses WHERE username = ? AND quiz_id = ?;`
 
 	// ReadResponseStatistics retrieves all Response statistics for a given Quiz from the Responses table.
-	// String Param 1: keyspace name
 	// Query Params: quiz_id
-	ReadResponseStatistics = `SELECT * FROM %s.responses
-WHERE quiz_id = ?;`
+	ReadResponseStatistics = `SELECT * FROM responses WHERE quiz_id = ?;`
 )
