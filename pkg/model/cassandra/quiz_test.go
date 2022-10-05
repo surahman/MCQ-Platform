aa8f8887156d1dcf57fdb57b09d8f0b1b81dd9e3
Saad Ur Rahman <saadurrahman@apache.org>
2022-10-05 01:02:19 -0400

    [Models] Cassandra validate Question

--- pkg/model/cassandra/quiz_test.go
+++ pkg/model/cassandra/quiz_test.go
@@ -1,5 +1,12 @@
 package model_cassandra
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/surahman/mcq-platform/pkg/validator"
+)
+
 var question1 = Question{Description: "Description of test 1",
 	Asset:   "http%3A%2F%2Fwww.url-encoded.web%2Fthis-is-url-encoded%2F",
 	Options: []string{"option 1", "option 2", "option 3", "option 4", "option 5"},
@@ -12,7 +19,7 @@ var question3 = Question{Description: "Description of test 3",
 	Options: []string{"option 1", "option 2", "option 3", "option 4", "option 5"},
 	Answers: []int32{1, 3}}
 var questionNotURLEnc = Question{Description: "Description of test 3",
-	Asset:   "http://www.url-encoded.web/this-is-url-encoded/",
+	Asset:   "http://www.url-encoded.web/this-is-url-encoded/<%az",
 	Options: []string{"option 1", "option 2", "option 3", "option 4", "option 5"},
 	Answers: []int32{1, 3}}
 var questionNoDesc = Question{Description: "",
@@ -46,3 +53,34 @@ var quizTooManyQuestions = QuizCore{Title: "Too many questions", MarkingType: "N
 var quizInvalidQuestions = QuizCore{Title: "Invalid questions", MarkingType: "Negative", Questions: []*Question{&questionNoDesc}}
 var quizTooManyAnswers = QuizCore{Title: "Too many answers", MarkingType: "Negative", Questions: []*Question{&questionTooManyAns}}
 var quizTooManyOpts = QuizCore{Title: "More answers than options", MarkingType: "Negative", Questions: []*Question{&questionAnsGTOpt}}
+
+func TestValidateQuestion(t *testing.T) {
+	testCases := []struct {
+		name          string
+		questionInput *Question
+		expectErr     require.ErrorAssertionFunc
+		expectedLen   int
+	}{
+		{"Valid question 1", &question1, require.NoError, 0},
+		{"Valid question 2", &question2, require.NoError, 0},
+		{"Valid question 3", &question3, require.NoError, 0},
+		{"Not URL encoded", &questionNotURLEnc, require.Error, 1},
+		{"No description", &questionNoDesc, require.Error, 1},
+		{"No options", &questionNoOpt, require.Error, 1},
+		{"Too many options", &questionTooManyOpt, require.Error, 1},
+		{"No answers in key", &questionNoAns, require.Error, 1},
+		{"Too many answers in key", &questionTooManyAns, require.Error, 1},
+		{"Out of range answers in key", &questionBadAns, require.Error, 1},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			err := validator.ValidateStruct(testCase.questionInput)
+			testCase.expectErr(t, err)
+
+			if err != nil {
+				require.Equal(t, testCase.expectedLen, len(err.(*validator.ErrorValidation).Errors))
+			}
+		})
+	}
+}
