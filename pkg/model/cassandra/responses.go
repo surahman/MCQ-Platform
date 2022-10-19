b5a381235d742323981fb6d6fdb1a06b28572a04
Saad Ur Rahman <saadurrahman@apache.org>
2022-10-19 19:05:56 -0400

    [Cassandra] paginated Stats request and response structs.

--- pkg/model/cassandra/responses.go
+++ pkg/model/cassandra/responses.go
@@ -18,3 +18,22 @@ type QuizResponse struct {
 	// The answer card to a quiz. The rows indices are the question numbers and the columns indices are the selected option numbers.
 	Responses [][]int32 `json:"responses,omitempty" cql:"responses" validate:"required,min=0,max=10,dive,min=0,max=5,dive,min=0,max=4"`
 }
+
+// StatsRequest is a request for statistics for a specific quiz.
+type StatsRequest struct {
+	QuizID     gocql.UUID
+	PageCursor string `json:"page_cursor"`
+	PageSize   int    `json:"page_size"`
+}
+
+// StatsResponse is a paginated response to a request for statistics for a specific quiz.
+type StatsResponse struct {
+	Records  []*Response `json:"records"`
+	Metadata struct {
+		QuizID     gocql.UUID `json:"quiz_id"`
+		NumRecords int        `json:"num_records"`
+	} `json:"metadata,omitempty"`
+	Links struct {
+		NextPage string `json:"next_page"`
+	} `json:"links,omitempty"`
+}
