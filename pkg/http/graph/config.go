145c55d5edb5220720bda411dcc64017653a2660
Saad Ur Rahman <saadurrahman@apache.org>
2022-11-16 11:43:05 -0500

    [GraphQL] refactored config loading to config loader.

--- pkg/http/graph/config.go
+++ pkg/http/graph/config.go
@@ -2,9 +2,8 @@ package graphql
 
 import (
 	"github.com/spf13/afero"
-	"github.com/spf13/viper"
+	"github.com/surahman/mcq-platform/pkg/config_loader"
 	"github.com/surahman/mcq-platform/pkg/constants"
-	"github.com/surahman/mcq-platform/pkg/validator"
 )
 
 // config is the configuration container for the HTTP GraphQL endpoint.
@@ -28,27 +27,5 @@ func newConfig() *config {
 
 // Load will attempt to load configurations from a file on a file system and then overwrite values using environment variables.
 func (cfg *config) Load(fs afero.Fs) (err error) {
-	viper.SetFs(fs)
-	viper.SetConfigName(constants.GetGraphQLFileName())
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(constants.GetEtcDir())
-	viper.AddConfigPath(constants.GetHomeDir())
-	viper.AddConfigPath(constants.GetBaseDir())
-
-	viper.SetEnvPrefix(constants.GetGraphQLPrefix())
-	viper.AutomaticEnv()
-
-	if err = viper.ReadInConfig(); err != nil {
-		return
-	}
-
-	if err = viper.Unmarshal(cfg); err != nil {
-		return
-	}
-
-	if err = validator.ValidateStruct(cfg); err != nil {
-		return
-	}
-
-	return
+	return config_loader.ConfigLoader(fs, cfg, constants.GetGraphQLFileName(), constants.GetGraphQLPrefix(), "yaml")
 }
