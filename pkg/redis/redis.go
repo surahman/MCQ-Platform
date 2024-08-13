092545aca0637062ae3fd1f460adb4e50ac91c71
Saad Ur Rahman <saadurrahman@apache.org>
2024-08-13 19:31:52 -0400

    [Redis] switched package import for updated path.

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -10,7 +10,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/go-redis/redis/v9"
+	"github.com/redis/go-redis/v9"
 	"github.com/spf13/afero"
 	"github.com/surahman/mcq-platform/pkg/logger"
 	"go.uber.org/zap"
